05302d69973424bb21e298451d5395b83995fcd4
TgQuocLinh <38097404+TgQuocLinh@users.noreply.github.com>
2018-08-24 11:44:22 +0700

    Update AppTest.java

--- src/test/java/com/mycompany/app/AppTest.java
+++ src/test/java/com/mycompany/app/AppTest.java
@@ -3,7 +3,7 @@ package com.mycompany.app;
 import java.io.ByteArrayOutputStream;
 import java.io.PrintStream;
 import org.junit.Before;
-import org.junit.Test
+import org.junit.Test;
 import org.junit.After;
 import static org.junit.Assert.*;
 
